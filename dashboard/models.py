f4e9839ebc53e5ab46830e156da342fa2355d9ae
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2022-07-26 21:25:50 +0530

    create book model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 
+class Book(models.Model):
+    bookID = models.CharField(max_length=10, null=True)
+    bookName = models.CharField(max_length=50, null=True)
+    author = models.CharField(max_length=50, null=True)
+    publishedDate = models.DateField(null=True)
+    category = models.CharField(max_length=20, null=True)
+    quantity = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.bookName} {self.category}'

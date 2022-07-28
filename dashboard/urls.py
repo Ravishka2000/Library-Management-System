564b70e266a6108d2cb54ad54d3269f9afc6aa71
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2022-07-28 15:02:16 +0530

    Create Login Register

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='dashboard-index'),
+    path('dashboard/', views.index, name='dashboard-index'),
     path('add/', views.addbook, name='book-add'),
     path('update/<str:pk>', views.updatebook, name='book-update'),
     path('delete/<str:pk>', views.deletebook, name='book-delete'),
--- libraryManagementSystem/settings.py
+++ libraryManagementSystem/settings.py
@@ -128,6 +128,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+MEDIA_ROOT = (BASE_DIR / "media")
+
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
@@ -135,3 +139,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard-index'
+
+LOGIN_URL = 'user-login'
